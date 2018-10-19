72f9906e281d9c6fc719be57a38befbac9e5a714
kamal885 <43897372+kamal885@users.noreply.github.com>
2018-10-19 17:42:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,28 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`${client.user.username}`, "https://twitch.tv//9ivv")
-    console.log('')
-    console.log('')
-    console.log('╔[════════════════════════════════════════════════════════════════]╗')
-    console.log(`[Start] ${new Date()}`);
-    console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-    console.log('')
-    console.log('╔[════════════════════════════════════]╗');
-    console.log(`Logged i as * [ " ${client.user.username} " ]`);
-    console.log('')
-    console.log('Informatins :')
-    console.log('')
-    console.log(`servers! [ " ${client.guilds.size} " ]`);
-    console.log(`Users! [ " ${client.users.size} " ]`);
-    console.log(`channels! [ " ${client.channels.size} " ]`);
-    console.log('╚[════════════════════════════════════]╝')
-    console.log('')
-    console.log('╔[════════════]╗')
-    console.log(' Bot Is Online')
-    console.log('╚[════════════]╝')
-    console.log('')
-    console.log('')
+const devs = ['487727064192122880' , '' , '' , ''];
+const adminprefix = "//";
+client.on('message', message => {
+    var argresult = message.content.split(`).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(:white_check_mark:   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(:white_check_mark:   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(:white_check_mark:   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.sendMessage(:white_check_mark:   ${argresult}`)
+  }
   });
 
 
